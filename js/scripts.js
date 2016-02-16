4d3cd88ecaadb77afc998417ed38d75109e5d6ff
Justin Mendoza <mendoj@gmail.com>
2016-02-15 23:10:51 -0500

    adding api script call from wikipedia

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
 
+  var search;
+
+var api = "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch="+ search+"Te&callback=JSON_CALLBACK";
+
+console.log(api);;
+  //var api = "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json"
+
+  $.getJSON(api, function(json) {
+    $(".results").html(JSON.stringify(json));
+    console.log(JSON.stringify(json));
+  });
 
 });
