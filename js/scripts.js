b3bffc02dcaeb6bf762ef553ba0523724725ea1e
Justin Mendoza <mendoj@gmail.com>
2016-06-09 11:37:27 -0400

    changed jquery code to angular code, still working on animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,51 @@
-$(document).ready(function() {
+angular
+  .module('wikipediaApp', ['ngAnimate']);
 
-  //var search="Washington";
+angular
+  .module('wikipediaApp')
+  .factory('wikipediaFactory', ['$http', function($http) {
 
-  $('#searchbtn').click(function(){
-    if($('#searchArticle').val) {
-      var search = $('#searchArticle').val();
-      $('.ulresults').html('');
-      $('#wikiAppContainer').removeClass("vertical-center");
-      $.ajax({
-        url: '//en.wikipedia.org/w/api.php',
-        data: {
-          action: 'query',
-          list: 'search',
-          srsearch: search,
-          format: 'json'
-        },
-        dataType: 'jsonp',
-        success: function (x) {
-          console.log('title', x.query.search);
-          console.log(x.query.search.length);
-          for (var i = 0; i < x.query.search.length; i++) {
-            $('.ulresults').html($('.ulresults').html()+"<li><h4>"+x.query.search[i].title+"</h4><p>"+x.query.search[i].snippet+"</p></li>");
-            console.log('title', x.query.search[i].title);
-          }
-        }
-      });
+    var wikipediaFactory = {};
+
+    var urlBase = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&continue=&generator=search&exchars=300&exlimit=10&exintro=1&explaintext=1&exsectionformat=plain&gsrsearch=";
+    var urlBack = "&gsrnamespace=0&gsrprop=snippet%7Csize&gsrlimit=10&callback=JSON_CALLBACK";
+
+    wikipediaFactory.getArticles = function(search) {
+      return $http.jsonp(urlBase + search + urlBack);
     }
 
-  });
+    return wikipediaFactory;
+
+  }]);
+
+angular
+  .module('wikipediaApp')
+  .controller('wikipediaController', ['$scope', 'wikipediaFactory',
+      function($scope, wikipediaFactory) {
+
+        $scope.articles = [];
+        var pageUrlbase = "https://en.wikipedia.org/?curid=";
+
+
+        $scope.search = function(searchInput) {
+
+          wikipediaFactory.getArticles(searchInput)
+            .then(function(response) {
+              //console.log(response.data.query.pages);
+              var values = response.data.query.pages;
+              angular.forEach(values, function(value, key) {
+                $scope.articles.push({
+                  title: value.title,
+                  url: pageUrlbase + value.pageid,
+                  body: value.extract
+                })
+              });
+              //console.log($scope.articles);
+            });
 
 
 
+        }
+        
 
-
-
-});
+  }]);
